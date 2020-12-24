bf411e30bcaa3498b127b97340c97ec819ddece2
Dinara Idriszade <dinara@getcaas.io>
2020-12-24 21:10:18 +0100

    Second commit

--- src/Deck.java
+++ src/Deck.java
@@ -1,32 +1,20 @@
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import org.apache.commons.io.FileUtils;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-
-import java.io.FileReader;
-import java.util.Iterator;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.TreeMap;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.parser.ParseException;
-
 public class Deck {
     List<Card> cards = new ArrayList<Card>();
-    ArrayList<String> descriptions = new ArrayList<String>();
-    ArrayList<String> titles = new ArrayList<String>();
 
     public Deck() throws IOException, ParseException {
-
-
 
 
     }
@@ -45,8 +33,8 @@ public class Deck {
         System.out.println("description: " + description);
         Card newcard = new Card(title, description);
         cards.add(newcard);
-        Writer output = null;
-        File file = new File("descriptions/" + cardTitle + ".txt");
+        Writer output;
+        File file = new File("cards/" + cardTitle + ".txt");
         output = new BufferedWriter(new FileWriter(file));
 
 
@@ -55,55 +43,33 @@ public class Deck {
             System.out.println("Using a custom serializer (in this case for a String): {}" + json);
 
 
-        //ouverture d'un flux de sortie vers le fichier "personne.serial"
-        //FileOutputStream fos = new FileOutputStream("descriptions/" + cardTitle + ".txt");
-
-        // création d'un "flux objet" avec le flux fichier
-       // ObjectOutputStream oos= new ObjectOutputStream(fos);
         output.write(json);
         output.close();
-        //output.writeObject(json);
-
-      /*  try {
-            // sérialisation : écriture de l'objet dans le flux de sortie
-            oos.writeObject(json);
-            // on vide le tampon
-            oos.flush();
-            System.out.println(json + " a ete serialise");
-        } finally {
-            //fermeture des flux
-            try {
-                oos.close();
-            } finally {
-                fos.close();
-            }
-        }*/
-
         System.out.println("addedcards: " + cards);
 
     }
 
     public void showCards() throws IOException, ParseException {
         JSONParser parser = new JSONParser();
-        File dir = new File("descriptions/");
+        File dir = new File("cards/");
         File[] directoryListing = dir.listFiles();
-        if (directoryListing != null) {
-            for (File child : directoryListing) {
-                String str = FileUtils.readFileToString(child, "UTF-8");
-                Object obj = parser.parse(str);
-                JSONObject jsonObject = (JSONObject) obj;
-                String title = String.valueOf(jsonObject.get("title"));
-                String description = String.valueOf(jsonObject.get("description"));
-                Card newcard = new Card(title, description);
-                cards.add(newcard);
-
-            }
-        } else {
-            // Handle the case where dir is not really a directory.
-            // Checking dir.isDirectory() above would not be sufficient
-            // to avoid race conditions with another process that deletes
-            // directories.
+        if (directoryListing != null && dir.isDirectory()) {
+                for (File child : directoryListing) {
+                    String str = FileUtils.readFileToString(child, "UTF-8");
+                    Object obj = parser.parse(str);
+                    JSONObject jsonObject = (JSONObject) obj;
+                    String title = String.valueOf(jsonObject.get("title"));
+                    String description = String.valueOf(jsonObject.get("description"));
+                    Card newcard = new Card(title, description);
+                    cards.add(newcard);
+                }
         }
+            else {
+                System.out.println("There are no cards!!!");
+
+                }
+
+
 
         for (Card card : cards) {
             System.out.println("title: " + card.title);
@@ -112,5 +78,50 @@ public class Deck {
         }
     }
 
+    public void deleteCard() throws IOException, ParseException {
+        showCards();
+        System.out.println("Choose the card you want to remove.");
+        Scanner scan = new Scanner(System.in);
+        String CardToRemove = scan.nextLine();
+        cards.removeIf(card -> card.title.equals(CardToRemove));
+        File file = new File("cards/" + CardToRemove + ".txt");
+        file.delete();
+
+    }
+
+
+    public void updateCard() throws IOException, ParseException {
+        showCards();
+        String newTitle, newDescription;
+        System.out.println("Choose the card you want to update");
+        Scanner scan = new Scanner(System.in);
+        String CardToUpdate = scan.nextLine();
+        for (Card card : cards) {
+            if (card.title.equals(CardToUpdate)){
+                System.out.println(card.title);
+                System.out.println(card.description);
+                newTitle = scan.nextLine();
+                newDescription = scan.nextLine();
+                card.title = newTitle;
+                card.description = newDescription;
+                var mapper = new ObjectMapper();
+                var json = mapper.writeValueAsString(card);
+                List<String> newLines = new ArrayList<>();
+                for (String line : Files.readAllLines(Paths.get("cards/" + CardToUpdate + ".txt"), StandardCharsets.UTF_8)) {
+                      newLines.add(line.replace(line,json));
+
+
+                }
+                Files.write(Paths.get("cards/" + CardToUpdate + ".txt"), newLines, StandardCharsets.UTF_8);
+                Path source = Paths.get("cards/" + CardToUpdate + ".txt");
+                Files.move(source, source.resolveSibling(newTitle + ".txt"));
+
+
+            }
+            System.out.println("title: " + card.title);
+            System.out.println("description: " + card.description);
+            System.out.println("card: " + card);
+        }
+    }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
 import org.json.simple.parser.ParseException;
-
 import java.io.IOException;
-
+import javax.swing.*;
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
         Deck deck = new Deck();
         //deck.addCard();
         //deck.showCards();
+         // deck.updateCard();
+        //deck.deleteCard();
         //deck.createCards();
         //Card card = new Card("bla");
         //card.whichFile();
+
+JFrame window = new JFrame("Mystic Tarot");
+window.setSize(600,300);
+window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+window.setVisible(true);
+
 
     }
 }
